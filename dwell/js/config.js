5b36f8c59db1b528b4c76b46e2d898b5a1608d59
sammadura <sammadura@gmail.com>
2026-08-22 02:52:48 -0700

    Add Dwell API config

---
+++ dwell/js/config.js
@@ -0,0 +1,4 @@
+window.DWELL = {
+  url: 'https://mlzhxenxavailzjtxwka.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1semh4ZW54YXZhaWx6anR4d2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MzUzNjgsImV4cCI6MjA5MzUxMTM2OH0.yAHLqulW4VuFgRUAnWvz30dL7mZRjX9CX-zZWPkpVdA'
+};
